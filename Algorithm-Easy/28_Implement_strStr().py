82447591e1b25bc3af2d363c5fb009db378d2efd
Jie Luo <jieluo@Jies-MacBook-Pro.local>
2018-08-28 15:23:56 -0400

    2018-08-28

--- Algorithm-Easy/28_Implement_strStr().py
+++ Algorithm-Easy/28_Implement_strStr().py
@@ -12,3 +12,23 @@ class Solution:
 
 if __name__ == '__main__':
     print(Solution().strStr("hello", "lo"))
+
+
+
+
+
+
+    """
+        Time Complexity = O(N*k), where k is the length of needle
+        Space Complexity = O(k)
+        Less Complexity?
+        https://github.com/kamyu104/LeetCode/blob/master/Python/implement-strstr.py
+
+        Implement strStr().
+        Return the index of the first occurrence of needle in haystack,
+        or -1 if needle is not part of haystack.
+
+        Example:
+        Input: haystack = "hello", needle = "ll"
+        Output: 2
+    """
